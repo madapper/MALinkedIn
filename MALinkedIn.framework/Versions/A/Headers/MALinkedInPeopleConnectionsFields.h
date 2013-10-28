a0ff3da6a7eed586494395278dccd6ff2b15898c
madapper <paul.napier@madapper.co.uk>
2013-10-28 22:29:54 +1100

    Fixed: Bug in MALinkedInPeopleConnections.m that would not allow use of name and location code at the same time

--- MALinkedIn.framework/Versions/A/Headers/MALinkedInPeopleConnectionsFields.h
+++ MALinkedIn.framework/Versions/A/Headers/MALinkedInPeopleConnectionsFields.h
@@ -22,11 +22,11 @@
 @property (nonatomic) BOOL field_r_basicprofile_phonetic_last_name;
 @property (nonatomic) BOOL field_r_basicprofile_formatted_phonetic_name;
 @property (nonatomic) BOOL field_r_basicprofile_headline;
-@property (nonatomic, retain) NSString *field_r_basicprofile_location_name;
-@property (nonatomic, retain) NSString *field_r_basicprofile_location_country_code; // Lower case values as defined by ISO 3166-1 alpha-2 standard. (http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements)
+@property (nonatomic) BOOL field_r_basicprofile_location_name;
+@property (nonatomic) BOOL field_r_basicprofile_location_country_code;
 @property (nonatomic) BOOL field_r_basicprofile_industry;
 @property (nonatomic) BOOL field_r_basicprofile_distance;
-@property (nonatomic) MALinkedInPeopleProfileFieldsDistance field_r_basicprofile_relation_to_viewer_distance;
+@property (nonatomic) BOOL field_r_basicprofile_relation_to_viewer_distance;
 @property (nonatomic) BOOL field_r_basicprofile_current_share;
 @property (nonatomic) BOOL field_r_basicprofile_num_connections;
 @property (nonatomic) BOOL field_r_basicprofile_num_connections_capped;
@@ -35,7 +35,7 @@
 @property (nonatomic) BOOL field_r_basicprofile_positions;
 @property (nonatomic) BOOL field_r_basicprofile_picture_url;
 @property (nonatomic) BOOL field_r_basicprofile_site_standard_profile_request;
-@property (nonatomic, retain) NSString *field_r_basicprofile_api_standard_profile_request_url;
+@property (nonatomic) BOOL field_r_basicprofile_api_standard_profile_request_url;
 @property (nonatomic) BOOL field_r_basicprofile_public_profile_url;
 @property (nonatomic) BOOL field_r_basicprofile_start;
 @property (nonatomic) BOOL field_r_basicprofile_count;
